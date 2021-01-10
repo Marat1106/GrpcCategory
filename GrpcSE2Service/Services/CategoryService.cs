ec804652ea4b13a8464e772277efdbdedf352645
Marat Mukiyat <m.mukiyat@astanait.edu.kz>
2021-01-10 20:08:23 +0600

    my project

--- GrpcSE2Service/Services/CategoryService.cs
+++ GrpcSE2Service/Services/CategoryService.cs
@@ -5,10 +5,23 @@ using System.Threading.Tasks;
 
 namespace GrpcService.Services
 {
+    
     public class CategoryService:Category.CategoryBase
     {
         private readonly Ilogger<CategoryService> _logger;
         private readonly ICategoryRepository _categoryRepository;
+        private readonly List<CategoryInfo> students = new List<CategoryInfo>
+        {
+            new CategoryInfo{User=new ProductInfo{id=1,name="Coco-cola",parentCategoryId=1},description="good product",price=200 },
+            new CategoryInfo{User=new ProductInfo{id=2,name="Fanta",parentCategoryId=2},description="good product",price=230 },
+            new CategoryInfo{User=new ProductInfo{id=3,name="Sprite",parentCategoryId=3},description="good product",price=280 },
+            new CategoryInfo{User=new ProductInfo{id=4,name="cola",parentCategoryId=4},description="good product",price=260 },
+            new CategoryInfo{User=new ProductInfo{id=5,name="Juice",parentCategoryId=5},description="good product",price=250 },
+            new CategoryInfo{User=new ProductInfo{id=6,name="Tea",parentCategoryId=6},description="good product",price=300 },
+
+
+        };
+        
 
 
         public CategoryService(Ilogger<CategoryService> logger, ICategoryRepository categoryRepository)
@@ -36,6 +49,18 @@ namespace GrpcService.Services
         public override Task GetProductsByCategoryId(CategoryLookUp request, IServiceStreamWriter<ProductInfo> responseStream, ServerCallContext context)
         {
             return base.GetProductsByCategoryId(request, responseStream, context);
+        }
+        public override async Task GetAllProducts(CategoryAllLookUp request,
+            IServerStreamWriter<CategoryInfo> responseStream, ServerCallContext context)
+        {
+            foreach (var Category in Categorys)
+            {
+                await Task.Delay(1000);
+                await responseStream.WriteAsync(Category);
+
+
+
+            }
         }
     }
 }
